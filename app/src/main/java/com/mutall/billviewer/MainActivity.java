cfe57b64f361b422df411276f4400d20a0591cd9
amshel <amshelhack3r@gmail.com>
2019-10-24 13:11:28 +0000

    add toolbar

--- app/src/main/java/com/mutall/billviewer/MainActivity.java
+++ app/src/main/java/com/mutall/billviewer/MainActivity.java
@@ -1,14 +1,19 @@
 package com.mutall.billviewer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
     }
 }
