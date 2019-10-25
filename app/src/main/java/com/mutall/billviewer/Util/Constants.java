84c37a6f96e443be4dd39f5162c91b4234c2e5bb
amshel <amshelhack3r@gmail.com>
2019-10-25 13:42:24 +0000

    Add our constants

--- app/src/main/java/com/mutall/billviewer/Util/Constants.java
+++ app/src/main/java/com/mutall/billviewer/Util/Constants.java
@@ -2,4 +2,7 @@ package com.mutall.billviewer.Util;
 
 public class Constants {
     public static final String MESSAGE = "message";
+    public static final int MY_PERMISSIONS_REQUEST_READ_SMS = 901;
+    public static final String SMS_LIST = "sms_list";
+    public static final String BUTTON = "button_value" ;
 }
