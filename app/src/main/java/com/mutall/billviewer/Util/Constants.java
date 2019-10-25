af3d60eae4980ea5489e3c61f6563257d66b2d10
amshel <amshelhack3r@gmail.com>
2019-10-25 14:48:02 +0000

    added a constants class

--- app/src/main/java/com/mutall/billviewer/Util/Constants.java
+++ app/src/main/java/com/mutall/billviewer/Util/Constants.java
@@ -3,6 +3,13 @@ package com.mutall.billviewer.Util;
 public class Constants {
     public static final String MESSAGE = "message";
     public static final int MY_PERMISSIONS_REQUEST_READ_SMS = 901;
+    public static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 902;
     public static final String SMS_LIST = "sms_list";
     public static final String BUTTON = "button_value" ;
+    public static final String ACCOUNTS_URL = "http://mutall.co.ke/mutall_rental?class=eaccount&method=get_all_eaccounts";
+    public static final String NOREADING_URL = "http://mutall.co.ke/android/details/client";
+    public static final String POST_BILLS = "http://mutall.co.ke/mutall_rental/index.php?class=msg&method=save_all";
+    public static final String TEST_URL = "http://mutall.co.ke/android/post.php";
+
+
 }
