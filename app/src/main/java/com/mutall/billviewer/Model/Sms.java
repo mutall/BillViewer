0939337d54c96dd6f6472f56b829fe4e9bafdfd9
amshel <amshelhack3r@gmail.com>
2019-10-25 13:43:29 +0000

    Add parcelable implementation

--- app/src/main/java/com/mutall/billviewer/Model/Sms.java
+++ app/src/main/java/com/mutall/billviewer/Model/Sms.java
@@ -1,6 +1,11 @@
 package com.mutall.billviewer.Model;
 
-public class Sms {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
+public class Sms implements Parcelable {
     private String number;
     private String message;
 
@@ -12,6 +17,23 @@ public class Sms {
         this.number = number;
         this.message = message;
     }
+
+    protected Sms(Parcel in) {
+        number = in.readString();
+        message = in.readString();
+    }
+
+    public static final Creator<Sms> CREATOR = new Creator<Sms>() {
+        @Override
+        public Sms createFromParcel(Parcel in) {
+            return new Sms(in);
+        }
+
+        @Override
+        public Sms[] newArray(int size) {
+            return new Sms[size];
+        }
+    };
 
     public String getNumber() {
         return number;
@@ -27,5 +49,24 @@ public class Sms {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("number: ").append(this.number).append("\n").append("body: ").append(this.message);
+       return builder.toString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(number);
+        parcel.writeString(message);
     }
 }
